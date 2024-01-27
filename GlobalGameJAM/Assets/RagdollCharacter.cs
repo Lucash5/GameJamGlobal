d4a3c2835115be86b90b1c622253f763d2caff30
Lucash5 <Lucas.Kajander@student.careeria.fi>
2024-01-27 13:39:32 +0200

    f<ghfzsghgszfzsf

---
+++ GlobalGameJAM/Assets/RagdollCharacter.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RagdollCharacter : MonoBehaviour
+{
+    bool a;
+    Rigidbody[] ragdollRigidbodies;
+    Animator anim;
+    public AudioClip[] screams;
+    AudioSource AS;
+
+    private void Awake()
+    {
+        AS = GetComponentInChildren<AudioSource>();
+        ragdollRigidbodies = GetComponentsInChildren<Rigidbody>();
+        anim = GetComponent<Animator>();
+
+        ToggleRigidbody(false);
+    }
+
+
+    public void ToggleRigidbody(bool t)
+    {
+        anim.enabled = t;
+
+        foreach (Rigidbody rigidbody in ragdollRigidbodies)
+        {
+            rigidbody.isKinematic = !t;
+            
+        }
+    }
+
+    public void screaming()
+    {
+        
+        if (a == false)
+        {
+            a = true;
+        AS.PlayOneShot(screams[Random.Range(0, screams.Length)]);
+        }
+    }
+}
--- GlobalGameJAM/Assets/Scripts/CarMovement.cs
+++ GlobalGameJAM/Assets/Scripts/CarMovement.cs
@@ -1,23 +1,91 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CarMovement : MonoBehaviour
 {
+    public Transform mickey;
+    public AudioClip[] audioclips;
+    AudioSource AS;
     public float speed;
+    public float maxSpeed = 200;
     public float steeringspeed;
     Rigidbody rb;
+    
+    public TMP_Text TMP;
 
+    public float currentSpeed; 
+
+    bool hasFlipped = false;
+
+    float inputX;
+    float inputZ = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
+
+        StartCoroutine(startcounting());
+
+
+        AS = GetComponent<AudioSource>();
         rb =  GetComponent<Rigidbody>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector3 (-1 * speed, rb.velocity.y, Input.GetAxisRaw("Horizontal") * steeringspeed);
+        mickey.rotation = new Quaternion(mickey.rotation.x, mickey.rotation.y - 0.0007f, mickey.rotation.z, mickey.rotation.w);
+        
+
+        inputX = Input.GetAxisRaw("Horizontal") * steeringspeed;
+
+        inputZ = Mathf.Clamp(inputZ, 0, maxSpeed);
+    }
+
+    /*void FixedUpdate()
+    {
+        if (!hasFlipped)
+        {
+            rb.velocity = new Vector3(inputZ * -1, rb.velocity.y, inputX);
+        }
+    }*/
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        RagdollCharacter rc = collision.gameObject.GetComponentInParent<RagdollCharacter>();
+
+        if (rc != null)
+        {
+            rc.ToggleRigidbody(true);
+            rc.screaming();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "Road")
+        {
+            inputZ += speed * Time.deltaTime;
+            rb.velocity = new Vector3(inputZ * -1, rb.velocity.y, inputX);
+        }
+    }
+
+
+    IEnumerator startcounting()
+    {
+
+        yield return new WaitForSeconds(3);
+        AS.PlayOneShot(audioclips[1]);
+        yield return new WaitForSeconds(2);
+        TMP.text = "Mickey : I think you can press the gas a little more now";
+        maxSpeed = 100;
+        AS.PlayOneShot(audioclips[0]);
 
     }
 }
